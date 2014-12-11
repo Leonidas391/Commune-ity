836c68073a91d540f8364942fed4322e1cf3ee38
mschumann15 <marcus.eshs@gmail.com>
2014-12-10 22:39:17 -0800

    Update index.php

--- index.php
+++ index.php
@@ -1,2 +1,16 @@
+<!DOCTYPE html>
+
+	<html lang="en">
+	
+		<head>
+			<link href="css/bootstrap.css" rel="stylesheet" type="text/css" />
+			
+		</head>
+			<body>
+				<h1> Hello World </h1>
+			</body>
+		
+	</html>
+
 <?php echo "hello world"; ?>
 
